7dfa3d2c126604f14965ea8fd158ca7b9ee76559
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-03-16 23:49:17 -0400

    Add files via upload

---
+++ TappingSpeakers.ino
@@ -0,0 +1,34 @@
+byte tapCount = 0;
+int led = 11;
+ int power;
+ int flip;
+void setup()
+{
+Serial.begin(9600);
+pinMode(led,OUTPUT);
+}
+
+void loop()
+{
+int sensorValue = analogRead(A0);
+
+
+if(sensorValue > 40) //detect a tap
+  {
+    delay(70);
+    tapCount = tapCount + 1; //increment tap count
+    int power = tapCount * 20;
+    flip = flip + 1;
+    if(tapCount > 4){
+ 
+  analogWrite(led, power); 
+  delay(1000);
+ 
+  Serial.println(power);
+    }
+    if(tapCount > 12){ //for some reason this if statement wont trigger. plz help
+      power = 0;
+    }
+    
+  }
+}
