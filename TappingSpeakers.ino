9ece8c6bb8cee5bb1a019f77a63b3584524b50f7
jamesbieboe12 <47893229+jamesbieboe12@users.noreply.github.com>
2019-04-12 08:17:42 -0400

    Update TappingSpeakers.ino

--- TappingSpeakers.ino
+++ TappingSpeakers.ino
@@ -1,31 +1,31 @@
+//speakers are hooked up to A0 when //tapped they produce a sharp electric //current they will serve as buttons
+int led = 11;
 byte tapCount = 0;
  int power;
-void setup()
-{
+void setup(){
 Serial.begin(9600);
 pinMode(led,OUTPUT);
 }
 
-void loop()
-{
+void loop(){
 int sensorValue = analogRead(A0);
-
-
-if(sensorValue > 40) //detect a tap
+//this is a tap
+ if(sensorValue > 40) //detect a tap
   {
     delay(70);
-    tapCount = tapCount + 1; //increment tap count
+    tapCount = tapCount + 1;  //increment tap count
     int power = tapCount * 20;
+ //the power the led gets is determined // by amount of taps
     if(tapCount > 4){
- 
   analogWrite(led, power); 
   delay(1000);
- 
   Serial.println(power);
+  //Shows analogWrite on monitor
     }
-    if(tapCount > 12){ //for some reason this if statement wont trigger. plz help
-      power = 0;
-    }
+      if(tapCount < 12){ 
+      tapCount = 0;
+//This line won't trigger
+      }
     
   }
 }
